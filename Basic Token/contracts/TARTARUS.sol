78cad4bda98960e93d8ada08826d7221776c1c84
varun upadhyay <varun.upadhyay911@gmail.com>
2022-01-13 22:51:17 +0530

    upgraded

--- Basic Token/contracts/TARTARUS.sol
+++ Basic Token/contracts/TARTARUS.sol
@@ -3,34 +3,60 @@
 pragma solidity ^0.8.4;
 
 contract TARTARUS {
+
+
     string public constant name = "TARTARUS";
     string public constant symbol = "TART";
     uint8 public constant decimal = 18;
-    uint256 public constant totalSupply = 10000000000000000000000000000;
 
-    mapping(address => uint256)balance; 
+
+    uint256 _totalSupply;
+
+
+    mapping (address => uint256) balances; 
+    mapping (address => mapping(address => uint256)) allowed;
+
 
     event Transfer(address _from, address _to, uint256 _amount);
+    event Approval(address _owner, address _spender, uint256 _amount);
 
-    constructor() payable {
-        balance[msg.sender] = totalSupply;
+
+    constructor() {
+        _totalSupply = 10000000000000000000000000000;
+        balances[msg.sender] = _totalSupply;
     }
+
 
     function balanceOf(address _owner) public view returns(uint256){
-        return balance[_owner];
+        return balances[_owner];
+    }
+    function allowance(address owner, address delegate) public view returns(uint256) {
+        return allowed[owner][delegate];
+    }
+    function totalSupply()public view returns(uint256) {
+        return _totalSupply;
     }
 
-    function transfer(address _reciver, uint256 _amount) public payable returns(bool){
-        require(balance[msg.sender] >= _amount, "Owner does not have required amount of token");
-        balance[msg.sender] = balance[msg.sender] - _amount;
-        balance[_reciver] = balance[_reciver] + _amount; 
+
+
+    function transfer(address _reciver, uint256 _amount) public returns(bool success){
+        require(balances[msg.sender] >= _amount, "Owner does not have required amount of token");
+        balances[msg.sender] = balances[msg.sender] - _amount;
+        balances[_reciver] = balances[_reciver] + _amount; 
         emit Transfer(msg.sender, _reciver, _amount);
         return true;
     }
-    function transferFrom(address _from, address _to, uint256 _amount) public payable returns(bool){
-        require(balance[_from] >= _amount, "Sender does not have required amount of token");
-        balance[_from] = balance[_from] - _amount;
-        balance[_to] = balance[_to] + _amount; 
+    function approve(address _delegate, uint256 _amount) public returns(bool success){
+        allowed[msg.sender][_delegate] = _amount;
+        emit Approval(msg.sender, _delegate, _amount);
+        return true;
+    }
+    function transferFrom(address _from, address _to, uint256 _amount) public returns(bool success){
+        require(balances[_from] >= _amount, "Sender does not have required amount of token");
+        require(_amount <= allowed[_from][msg.sender], "Sender does not have required allowance");
+        balances[_from] = balances[_from] - _amount;
+        allowed[_from][msg.sender] = allowed[_from][msg.sender] - _amount;
+        balances[_to] = balances[_to] + _amount; 
         emit Transfer(_from, _to, _amount);
         return true;
     }
--- Basic Token/test/testcases.js
+++ Basic Token/test/testcases.js
@@ -33,35 +33,51 @@ contract("TARTARUS", () => {
     });
 
     it("Should give balance of given address", async () => {
-        await instance.transfer("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7", 100000)
+        await instance.transfer("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7", 1000000)
         //const result = await instance.balanceOf("0x6C34498E696C55401c9BfAE12dA1589B42449275");
         const result_1 = await instance.balanceOf("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7");
-        assert(result_1.toNumber(), "100000");
-    })
+        assert.equal(result_1.toNumber(), 1000000);
+    });
 
-    it("Should transfer amount from sender to ownwe", async () => {
+    it("Should transfer amount from sender to owner", async () => {
         try{
-            const result = await instance.transferFrom("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7", "0xD13C5AFF45876b75E294C985532Ed86427253e9C", 1000)
+            await instance.approve("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7", 100000);
+            const result = await instance.transferFrom("0x6C34498E696C55401c9BfAE12dA1589B42449275", "0xD13C5AFF45876b75E294C985532Ed86427253e9C", 10000,
+            {from :"0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7"});
+
             const result1 = await instance.balanceOf("0xD13C5AFF45876b75E294C985532Ed86427253e9C");
             const result2 = await instance.balanceOf("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7");
-            assert(result, true);
-            assert(result1.toNumber(), 1000);
-            assert(result2.toNumber(), 99000);
+            
+            assert(result);
+            assert.equal(result1.toNumber(), 10000);
+            assert.equal(result2.toNumber(), 1000000);
         }catch(error){
-            assert.equal(error.message, "Sender does not have required amount of token");
-        }
+            assert.equal(error.message, "reverted", "Sender does not have required amount of token");
+        };
+    });
+
+    it("Should return the allowance for the delegate", async () => {
+        await instance.approve("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7", 10000000);
+        const result  = await instance.allowance("0x6C34498E696C55401c9BfAE12dA1589B42449275", "0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7");
+        assert.equal(result.toNumber(), 10000000);
     });
 
     it("Should transfer amount from owner to reciver address", async () => {
         try{
-            const result = await instance.transfer("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7", 10000)
-            //const result1 = await instance.balanceOf("0x6C34498E696C55401c9BfAE12dA1589B42449275");
-            const result2 = await instance.balanceOf("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7");
-            assert(result, true);
-            //assert(result1.toString(), "");
-            assert(result2.toNumber(), 10000);
+            await instance.transfer("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7", 10000);
+            //const result = await instance.balanceOf("0x6C34498E696C55401c9BfAE12dA1589B42449275");
+            const result = await instance.balanceOf("0x3796Fc977a2156b080ADE776Fa5b1dDf16624bc7");
+            //assert.equal(result.toNumber(), 110000);
+            assert.equal(result.toNumber(), 1010000);
         }catch(error){
             assert.equal(error.message, "Owner does not have required amount of token");
-        }
+        };
     });
+
+    it("Should check for the return of approve function", async () => {
+        await instance.approve("0xD13C5AFF45876b75E294C985532Ed86427253e9C", 1000);
+        const result  = await instance.allowance("0x6C34498E696C55401c9BfAE12dA1589B42449275", "0xD13C5AFF45876b75E294C985532Ed86427253e9C");
+        assert.equal(result.toNumber(), 1000);
+    });
+
 });
